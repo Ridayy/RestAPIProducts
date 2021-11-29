65474d0d31b7b326b4d6690967f44aca99c3c072
Rida Arif <ridaarif20@gmail.com>
2021-11-29 18:57:08 +0500

    "Updated"

---
+++ WebApplication2/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WebApplication2.Models
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public string productName { get; set; }
+        public string productDescription { get; set; }
+        public float productPrice { get; set; }
+        public DateTime dateCreated { get; set; } = DateTime.Now;
+        public bool isDeleted { get; set; } = false;
+    }
+}
